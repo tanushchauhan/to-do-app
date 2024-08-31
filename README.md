0d3a38a172a5721f0305ffaca0b7eaa9c3cabc4c
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-31 16:35:57 -0500

    Finishing touches. Added README.

---
+++ README.md
@@ -0,0 +1,31 @@
+# To Do App
+
+This is a simple to-do application that I made using HTML, CSS, and Javascript.
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/tanushchauhan/to-do-app.git
+```
+
+Go to the project directory
+
+```bash
+  cd to-do-app
+```
+
+Open the index.html file in your favorite browser and enjoy!
+
+## Authors
+
+- [@tanushchauhan](https://github.com/tanushchauhan/)
+
+## Screenshots
+
+![App Screenshot](https://raw.githubusercontent.com/tanushchauhan/to-do-app/main/screenshots/1.png)
+
+![App Screenshot](https://raw.githubusercontent.com/tanushchauhan/to-do-app/main/screenshots/2.png)
+
+![App Screenshot](https://raw.githubusercontent.com/tanushchauhan/to-do-app/main/screenshots/3.png)
--- script.js
+++ script.js
@@ -17,7 +17,21 @@ const addItem = function (e) {
   const content = item.value;
   if (arrDivs.length === 0) {
     const html = `<div class="DIV" id="0-Divs"><button class="btnRemove" id="0-btn">❌</button>
-      <li class="actual-list-item">${content}</li></div>`;
+    <li class="actual-list-item">${content}</li></div>`;
     listItems.insertAdjacentHTML("beforeend", html);
+    arrBtns.push(0);
+    arrDivs.push(0);
+    document.getElementById("0-btn").addEventListener("click", removeItem);
+  } else {
+    const html = `<div class="DIV" id="${arrDivs.length}-Divs"><button class="btnRemove" id ="${arrBtns.length}-btn">❌</button>
+  <li class="actual-list-item">${content}</li></div>`;
+    listItems.insertAdjacentHTML("beforeend", html);
+    document
+      .getElementById(`${arrDivs.length}-btn`)
+      .addEventListener("click", removeItem);
+    arrBtns.push(arrBtns.length);
+    arrDivs.push(arrDivs.length);
   }
 };
+
+btnform.addEventListener("click", addItem);
